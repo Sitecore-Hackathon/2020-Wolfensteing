6cd66563ea13a6034bdb9bf1cf920a45032973ae
Jorge <jorgejlcmjr@hotmail.com>
2020-02-29 10:02:04 -0500

    Added hero timer

--- src/Feature/HackatonWeb.Feature.Hero/Models/HeroContent.cs
+++ src/Feature/HackatonWeb.Feature.Hero/Models/HeroContent.cs
@@ -17,7 +17,9 @@ namespace HackatonWeb.Feature.Hero.Models
         public string BackgroundImage { get; set; }
 
         public string VideoUrl { get; set; }
-
+        
         public HtmlString MainEventDate { get; set; }
+
+        public HtmlString LabelDaysLeft { get; set; }
     }
 }
