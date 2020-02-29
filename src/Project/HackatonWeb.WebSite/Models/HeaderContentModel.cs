d81b6bac11585051fd6b8bcdd80a0b2e17328a2e
Ramiro Francisco Batallas <ramiro.batallas@verndale.com>
2020-02-29 14:48:02 -0500

    header in general

---
+++ src/Project/HackatonWeb.WebSite/Models/HeaderContentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackatonWeb.WebSite.Models
+{
+    public class HeaderContentModel
+    {
+        public HtmlString Logo { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/Project/HackatonWeb.WebSite/Models/NavigationMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackatonWeb.WebSite.Models
+{
+    public class NavigationMenu
+    {
+        public string MenuId { get; set; }
+        public string Title { get; set; }
+
+        public HtmlString MenuLink { get; set; }
+
+        public List<NavigationMenu> SubMenus { get; set; }
+
+        public NavigationMenu()
+        {
+            SubMenus = new List<NavigationMenu>();
+        }
+    }
+}
