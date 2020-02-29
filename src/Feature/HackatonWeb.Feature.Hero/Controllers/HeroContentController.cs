0bd5385c51eb0fb32ae9ff9c3af875b7b368be57
Jorge <jorgejlcmjr@hotmail.com>
2020-02-29 02:29:13 -0500

    Add foundation project

--- src/Feature/HackatonWeb.Feature.Hero/Controllers/HeroContentController.cs
+++ src/Feature/HackatonWeb.Feature.Hero/Controllers/HeroContentController.cs
@@ -1,10 +1,7 @@
 ﻿using HackatonWeb.Feature.Hero.Models;
-using Sitecore.Data.Fields;
+using HackatonWeb.Foundation.Util;
 using Sitecore.Mvc.Presentation;
 using Sitecore.Web.UI.WebControls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
@@ -30,39 +27,10 @@ namespace HackatonWeb.Feature.Hero.Controllers
                 LinkMoreInformation = new HtmlString(FieldRenderer.Render(item, "LinkMoreInformation")),
                 LinkJoin = new HtmlString(FieldRenderer.Render(item, "LinkJoin")),
                 BackgroundImage = new HtmlString(FieldRenderer.Render(item, "BackgroundImage")),
-                VideoUrl = LinkUrl(item.Fields["VideoUrl"])
+                VideoUrl = LinkUtil.GetUrlFromLinkField(item.Fields["VideoUrl"])
             };
 
             return heroContent;
-        }
-
-        private string LinkUrl(LinkField lf)
-        {
-            switch (lf.LinkType.ToLower())
-            {
-                case "internal":
-                    // Use LinkMananger for internal links, if link is not empty
-                    return lf.TargetItem != null ? Sitecore.Links.LinkManager.GetItemUrl(lf.TargetItem) : string.Empty;
-                case "media":
-                    // Use MediaManager for media links, if link is not empty
-                    return lf.TargetItem != null ? Sitecore.Resources.Media.MediaManager.GetMediaUrl(lf.TargetItem) : string.Empty;
-                case "external":
-                    // Just return external links
-                    return lf.Url;
-                case "anchor":
-                    // Prefix anchor link with # if link if not empty
-                    return !string.IsNullOrEmpty(lf.Anchor) ? "#" + lf.Anchor : string.Empty;
-                case "mailto":
-                    // Just return mailto link
-                    return lf.Url;
-                case "javascript":
-                    // Just return javascript
-                    return lf.Url;
-                default:
-                    // Just please the compiler, this
-                    // condition will never be met
-                    return lf.Url;
-            }
-        }
+        }  
     }
 }
