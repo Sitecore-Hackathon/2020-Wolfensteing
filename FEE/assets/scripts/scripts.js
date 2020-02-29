1cef55689e42e8bc4ed9d52338829599973d103d
Marcelo Bonilla <marcelo.bonilla@verndale.com>
2020-02-29 13:48:12 -0500

    add suscribe functionality

--- FEE/assets/scripts/scripts.js
+++ FEE/assets/scripts/scripts.js
@@ -145,7 +145,7 @@ var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11',
 center: [-77.04, 38.907],
-zoom: 1
+zoom: 1.5
 });
  
 map.on('load', function() {
@@ -232,6 +232,8 @@ map.on('mouseleave', 'places', function() {
   popup.remove();
 });
 
+map.scrollZoom.disable();
+
 
 });
 
@@ -299,6 +301,24 @@ map.on('mouseleave', 'places', function() {
           if (backdrop !== null) document.body.removeChild(backdrop);
       }
   };
+
+  $('.suscribe *[type="submit"]').click(function(e){
+    e.preventDefault();
+    var email = $('.suscribe *[type="text"]').val();
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    var url = $('.suscribe form').attr('action')+email;
+    if(pattern.test(email)){
+      $.get(url, function(data, status){
+        if(data.Seccess){
+          $('.suscribe .message').addClass('success').html(data.Message);
+        }else{
+          $('.suscribe .message').addClass('error').html(data.Message);
+        }
+      });
+    }else{
+      $('.suscribe .message').addClass('error').html('Invalid Email.')
+    }
+  })
 
  
  
