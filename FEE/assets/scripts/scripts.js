f23bebe2d93ef64c70f2d85e61e87732a64d2bc6
Marcelo Bonilla <marcelo.bonilla@verndale.com>
2020-02-29 07:59:23 -0500

    FEE tweets

--- FEE/assets/scripts/scripts.js
+++ FEE/assets/scripts/scripts.js
@@ -106,7 +106,7 @@ function passTweet(){
 }
 
     // Set the date we're counting down to
-var countDownDate = new Date("03/27/2020 08:00 GMT+05:00").getTime();
+var countDownDate = new Date(document.getElementById("timer").getAttribute('data-date')).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
